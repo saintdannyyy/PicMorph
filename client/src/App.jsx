a199da9a5b6ee5d2912444463e729526cdca7ae8
Saintdannyyy <danieltesla746@gmail.com>
2025-03-21 21:09:31 +0000

    Refactor App component layout to include Header and Footer, enhancing structure and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import UploadImage from "./components/Upload";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <UploadImage />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <Header />
+      <div className="flex flex-grow items-center justify-center w-full">
+        <UploadImage />
+      </div>
+      <Footer />
     </div>
   );
 }
