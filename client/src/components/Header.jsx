d132c90fe61cc4d40875fd2313de686c087aef70
Saintdannyyy <danieltesla746@gmail.com>
2025-03-21 23:10:04 +0000

    Enhance header component: adjust width, apply custom font style, and improve layout for better visual appeal

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="fixed top-1 w-[80%] bg-gradient-to-r from-purple-600 to-indigo-500 shadow-md py-4 flex justify-center items-center rounded-md">
-      <h1 className="text-white text-xl font-bold tracking-wide">PicMorph</h1>
+    <header className="fixed top-1 w-[60%] bg-gradient-to-r from-purple-600 to-indigo-500 shadow-md py-4 flex justify-center items-center rounded-md">
+      <h1
+        className="text-white text-xl font-bold"
+        style={{ fontFamily: "Lobster, cursive" }}
+      >
+        PicMorph
+      </h1>
     </header>
   );
 };
