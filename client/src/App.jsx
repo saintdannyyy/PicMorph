2ce2e3fda87e52763f17365848f3fb4faaa1a774
Saintdannyyy <danieltesla746@gmail.com>
2025-03-22 00:12:45 +0000

    vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,9 @@ import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <Analytics />
       <Header />
       <div className="flex flex-grow items-center justify-center w-full">
+        <Analytics />
         <UploadImage />
       </div>
       <Footer />
