c37f5cf47e7150c972056e2222d9e36c76b18e58
Saintdannyyy <danieltesla746@gmail.com>
2025-03-22 02:40:12 +0000

    Update Header and Footer components: change background color from blue to black for a consistent theme

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="fixed w-[70%] top-1 bg-blue-600 shadow-md py-4 flex justify-center items-center rounded-md">
+    <header className="fixed w-[70%] top-1 bg-black shadow-md py-4 flex justify-center items-center rounded-md">
       <h1
         className="text-white font-bold text-2xl"
         style={{ fontFamily: "Lobster, cursive" }}
