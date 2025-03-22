5cbb4d31e5fe5f1261d85dcb66bb033c4c28c773
Saintdannyyy <danieltesla746@gmail.com>
2025-03-22 00:24:25 +0000

    Fix import path for SpeedInsights component in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import UploadImage from "./components/Upload";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
